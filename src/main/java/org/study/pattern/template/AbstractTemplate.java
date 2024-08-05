de0413dccedb18987056d1708fefc474307561cb
dongjun6343 <dongjun6343@naver.com>
2024-08-05 18:48:03 +0900

    :sparkles: 템플릿 메서드 패턴

--- src/main/java/org/study/pattern/template/AbstractTemplate.java
+++ src/main/java/org/study/pattern/template/AbstractTemplate.java
@@ -1,8 +1,27 @@
-package org.study.pattern.test;
+package org.study.pattern.template;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public abstract class AbstractTemplate {
 
+    // 변하지 않는 부분
     public void execute() {
-        
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        //log.info("비즈니스 로직1 실행");
+        call(); // 상속
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
     }
+
+    // 변하는 부분
+    protected abstract void call();
+
+    /**
+     * 부모 클래스에 변하지 않는 템플릿 코드를 두고,
+     * 변하는 부분은 자식 클래스에 두고 상속과 오버라이딩을 사용해서 처리한다.
+     */
 }
--- src/main/java/org/study/pattern/template/TemplateMethodMain.java
+++ src/main/java/org/study/pattern/template/TemplateMethodMain.java
@@ -3,11 +3,12 @@ package org.study.pattern.template;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class TemplateMethodMainV0 {
+public class TemplateMethodMain {
 
     public static void main(String[] args) {
-        logic1();
-        logic2();
+//        logic1();
+//        logic2();
+        templateMethod();
     }
 
 
@@ -29,5 +30,17 @@ public class TemplateMethodMainV0 {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 사용
+     * 상속 + 오버라이딩
+     */
+    private static void templateMethod() {
+        AbstractTemplate template = new SubClassLogic1();
+        template.execute();
+
+        AbstractTemplate template1 = new SubClassLogic2();
+        template1.execute();
     }
 }
