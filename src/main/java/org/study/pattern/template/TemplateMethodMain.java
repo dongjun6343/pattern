4c360b67a319470030d2fd9dbe7ad766e545e733
dongjun6343 <dongjun6343@naver.com>
2024-08-05 18:53:45 +0900

    :sparkles: 템플릿 메서드 패턴 - 익명 내부 클래스로 변경

--- src/main/java/org/study/pattern/template/TemplateMethodMain.java
+++ src/main/java/org/study/pattern/template/TemplateMethodMain.java
@@ -8,7 +8,8 @@ public class TemplateMethodMain {
     public static void main(String[] args) {
 //        logic1();
 //        logic2();
-        templateMethod();
+//        templateMethodV1();
+        templateMethodV2();
     }
 
 
@@ -35,12 +36,40 @@ public class TemplateMethodMain {
     /**
      * 템플릿 메서드 패턴 사용
      * 상속 + 오버라이딩
+     *
+     * 다형성을 사용해서 변하는 부분과 변하지 않는 부분을 분리하는 방법.
+     *
+     * 하지만, 템플릿 메서드 패턴은 SubClassLogic1 , SubClassLogic2를 계속 만들어야 하는 단점이 있다
+     * => 어떻게 해결할까? => 익명 내부 클래스 사용해보자!
      */
-    private static void templateMethod() {
+    private static void templateMethodV1() {
         AbstractTemplate template = new SubClassLogic1();
         template.execute();
 
         AbstractTemplate template1 = new SubClassLogic2();
         template1.execute();
     }
+
+
+    /**
+     * 익명 내부 클래스 => 변경
+     */
+    private static void templateMethodV2() {
+        AbstractTemplate template = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+        template.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+        template2.execute();
+    }
+
 }
