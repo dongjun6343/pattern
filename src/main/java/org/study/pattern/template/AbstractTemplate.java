4a0db2195a8e053fe9ee6b1ab1853fb179afc1fc
dongjun6343 <dongjun6343@naver.com>
2024-08-05 18:47:45 +0900

    :sparkles: 템플릿 메서드 패턴

---
+++ src/main/java/org/study/pattern/template/AbstractTemplate.java
@@ -0,0 +1,8 @@
+package org.study.pattern.test;
+
+public abstract class AbstractTemplate {
+
+    public void execute() {
+        
+    }
+}
---
+++ src/main/java/org/study/pattern/template/TemplateMethodMain.java
@@ -0,0 +1,33 @@
+package org.study.pattern.template;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TemplateMethodMainV0 {
+
+    public static void main(String[] args) {
+        logic1();
+        logic2();
+    }
+
+
+    private static void logic1() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private static void logic2() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
